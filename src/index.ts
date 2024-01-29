29df76f95c9836fcd3d245f5dc82859f80fdc49d
Mohamed Alabasy <eng.mohamed.alabasy@gmail.com>
2024-01-29 20:19:43 +0200

    add notFoundMiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import express from 'express';
 import 'dotenv/config'
 
+import notFoundMiddleware from './middleware/notFoundMiddleware';
 
 import routes from './routes/routes';
+
 const app = express();
+// #=======================================================================================#
+// #			                            router                                         #
+// #=======================================================================================#
+app.use('', routes);
+// #=======================================================================================#
+// #			                      not Found middleware                                 #
+// #=======================================================================================#
+app.use(notFoundMiddleware);
 
 export default app;
