305b9c9ca2cc6c0f9dbf282f1fa349f3ebea8b36
Mohamed Alabasy <eng.mohamed.alabasy@gmail.com>
2024-01-29 20:24:20 +0200

    add errorMiddleware

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,14 @@ import cors from 'cors';
 
 import morganMiddleware from './middleware/morganMiddleware';
 import notFoundMiddleware from './middleware/notFoundMiddleware';
+import errorMiddleware from './middleware/errorMiddleware';
 
 import routes from './routes/routes';
 
 const app = express();
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`App Run to http://${process.env.HOST}:${process.env.PORT || 8080}`);
+});
 // #=======================================================================================#
 // #			                            body_parse                                     #
 // #=======================================================================================#
@@ -26,5 +30,10 @@ app.use('', morganMiddleware, routes);
 // #			                      not Found middleware                                 #
 // #=======================================================================================#
 app.use(notFoundMiddleware);
+// #=======================================================================================#
+// #			                       error middleware                                    #
+// #=======================================================================================#
+app.use(errorMiddleware);
+
 
 export default app;
