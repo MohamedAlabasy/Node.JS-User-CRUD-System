0b01382cb30055dc073363436ababa072399bb8c
Mohamed Alabasy <eng.mohamed.alabasy@gmail.com>
2024-01-29 20:20:32 +0200

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import 'dotenv/config'
+import cors from 'cors';
 
 import notFoundMiddleware from './middleware/notFoundMiddleware';
 
 import routes from './routes/routes';
 
 const app = express();
+// #=======================================================================================#
+// #			                     add header or use cors                                #
+// #=======================================================================================#
+app.use(cors());
 // #=======================================================================================#
 // #			                            router                                         #
 // #=======================================================================================#
